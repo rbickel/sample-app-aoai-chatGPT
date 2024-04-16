997d4fae4825ec0eedacba7de8d9d0308a3b4d5f
Raphael Bickel <raphael.bickel@gmail.com>
2024-04-16 13:29:07 +0000

    ui improvments

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -690,7 +690,7 @@ const Chat = () => {
                                         <div className={styles.chatMessageGpt}>
                                             <Answer
                                                 answer={{
-                                                    answer: "Recherche de votre réponse...",
+                                                    answer: "Activation FTL... Saut hyperespace dans 3... 2... 1...",
                                                     citations: []
                                                 }}
                                                 onCitationClicked={() => null}
@@ -792,7 +792,7 @@ const Chat = () => {
                             </Stack>
                             <h5 className={styles.citationPanelTitle} tabIndex={0} title={activeCitation.url ? activeCitation.url : activeCitation.title ?? ""} onClick={() => onViewSource(activeCitation)}>{activeCitation.title}</h5>
                             {activeCitation.url && activeCitation.url?.includes('.pdf') && (
-                                <iframe  src={activeCitation.url} className={styles.citationPanelImage} width="100%" height="100%"/>
+                                <iframe  src={activeCitation.url+'#page=1'} className={styles.citationPanelImage} width="100%" height="100%"/>
                             )}
                             {activeCitation.url && !activeCitation.url?.includes('.pdf') && (
                                 <img  src={activeCitation.url} className={styles.citationPanelImage} width="100%"/>
